ac7e24c3a8b2e973fe3e73ee6696a83dd537245b
Nicolas Sosa <workoanda@gmail.com>
2024-11-18 16:37:31 -0300

    Add name field to model structs

--- models/expenseCategory.go
+++ models/expenseCategory.go
@@ -3,5 +3,6 @@ package models
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type ExpenseCategory struct {
-	Id primitive.ObjectID	`json:"id" bson:"_id"`
+	Id 	 	primitive.ObjectID	`json:"id" bson:"_id"`
+	Name	string				`json:"name" bson:"name"`
 }
--- models/expensePaymentMethod.go
+++ models/expensePaymentMethod.go
@@ -3,5 +3,6 @@ package models
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type ExpensePaymentMethod struct {
-	Id primitive.ObjectID	`json:"id" bson:"_id"`
+	Id 		primitive.ObjectID	`json:"id" bson:"_id"`
+	Name	string				`json:"name" bson:"name"`
 }
