7b657e374c590ddaeb19fdf2f8c335201ce845f8
Nicolas Sosa <workoanda@gmail.com>
2024-11-24 18:28:25 -0300

    Add func getMongoDBCollections

--- db/mongoDBConnection.go
+++ db/mongoDBConnection.go
@@ -11,8 +11,7 @@ import (
 )
 
 // File: mongoDBConnection.go
-// Open new connection
-func SetupMongoDB() (*mongo.Collection, *mongo.Collection, *mongo.Client, context.Context, context.CancelFunc) {
+func SetupMongoDB() (*mongo.Client, context.Context, context.CancelFunc) {
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
 	if err != nil {
@@ -22,12 +21,17 @@ func SetupMongoDB() (*mongo.Collection, *mongo.Collection, *mongo.Client, contex
 	if err != nil {
 		panic(fmt.Sprintf("Mongo DB ping issue %s", err))
 	}
-	expense_categoriesCollection := client.Database("expenses-tracker").Collection("expense_categories")
-	expense_payment_methodsCollection := client.Database("expenses-tracker").Collection("expense_payment_methods")
-	return expense_categoriesCollection, expense_payment_methodsCollection, client, ctx, cancel
+	
+	return client, ctx, cancel
 }
 
-// Close the connection
+func getMongoDBCollections(client *mongo.Client) (*mongo.Collection, *mongo.Collection) {
+	expense_categoriesCollection := client.Database("expenses-tracker").Collection("expense_categories")
+	expense_payment_methodsCollection := client.Database("expenses-tracker").Collection("expense_payment_methods")
+
+	return expense_categoriesCollection, expense_payment_methodsCollection
+}
+
 func CloseConnection(client *mongo.Client, context context.Context, cancel context.CancelFunc) {
 	defer func() {
 		cancel()
