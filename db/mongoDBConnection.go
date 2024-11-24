2492e62afbb97fa267d000f8558eecfa3227e2af
Nicolas Sosa <workoanda@gmail.com>
2024-11-24 16:53:51 -0300

    Addapt mongoDBConnection.go to this project

--- db/mongoDBConnection.go
+++ db/mongoDBConnection.go
@@ -12,7 +12,7 @@ import (
 
 // File: mongoDBConnection.go
 // Open new connection
-func SetupMongoDB() (*mongo.Collection, *mongo.Client, context.Context, context.CancelFunc) {
+func SetupMongoDB() (*mongo.Collection, *mongo.Collection, *mongo.Client, context.Context, context.CancelFunc) {
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
 	if err != nil {
@@ -22,8 +22,9 @@ func SetupMongoDB() (*mongo.Collection, *mongo.Client, context.Context, context.
 	if err != nil {
 		panic(fmt.Sprintf("Mongo DB ping issue %s", err))
 	}
-	collection := client.Database("mongo-golang-test").Collection("Users")
-	return collection, client, ctx, cancel
+	expense_categoriesCollection := client.Database("expenses-tracker").Collection("expense_categories")
+	expense_payment_methodsCollection := client.Database("expenses-tracker").Collection("expense_payment_methods")
+	return expense_categoriesCollection, expense_payment_methodsCollection, client, ctx, cancel
 }
 
 // Close the connection
