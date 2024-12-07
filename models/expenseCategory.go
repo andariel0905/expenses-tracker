703f82e8ca74cbe1d39f2de5fa199371e2ae9b49
Andariel <workoanda@gmail.com>
2024-12-06 22:27:06 -0300

    Delete Id property in ExpenseCategory model

--- models/expenseCategory.go
+++ models/expenseCategory.go
@@ -1,8 +1,6 @@
 package models
 
-import "go.mongodb.org/mongo-driver/bson/primitive"
-
 type ExpenseCategory struct {
-	Id 	 	primitive.ObjectID	`json:"id" bson:"_id"`
-	Name	string				`json:"name" bson:"name"`
+	//	Id   primitive.ObjectID `json:"id" bson:"_id"`
+	Name string `json:"name" bson:"name"`
 }
