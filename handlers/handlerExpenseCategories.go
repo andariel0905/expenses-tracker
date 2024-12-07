71bdbf5a855f113c1cc44eb539a6b9f5764bb6fa
Andariel <workoanda@gmail.com>
2024-12-06 22:21:40 -0300

    Create post method in expenseCategories handler

--- handlers/handlerExpenseCategories.go
+++ handlers/handlerExpenseCategories.go
@@ -1,1 +1,24 @@
 package handlers
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/andariel0905/expenses-tracker/db"
+	"github.com/andariel0905/expenses-tracker/models"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+func postExpenseCategory(client *mongo.Client, cxt context.Context, newExpenseCategoryName string) {
+	collection := db.GetMongoDBCollection(client, "expenseCategories")
+
+	newExpenseCategory := models.ExpenseCategory{newExpenseCategoryName}
+
+	insertResult, err := collection.InsertOne(cxt, newExpenseCategory)
+
+	if err != nil {
+		panic(fmt.Sprintf("Doc insert issue %s", err))
+	}
+
+	fmt.Println("Inserted a single document: ", insertResult.InsertedID)
+}
