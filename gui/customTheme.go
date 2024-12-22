cf9b515ac7c81998b8ba5cee115c72744dc3bcb1
Andariel <workoanda@gmail.com>
2024-12-22 02:22:39 -0300

    Initialize customTheme.go to set a custom theme for the GUI

---
+++ gui/customTheme.go
@@ -0,0 +1,33 @@
+package gui
+
+import (
+	"image/color"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/theme"
+)
+
+type MyTheme struct{}
+
+func (m MyTheme) Color(name fyne.ThemeColorName, variant fyne.ThemeVariant) color.Color {
+	if name == theme.ColorNameBackground {
+		if variant == theme.VariantLight {
+			return color.White
+		}
+		return color.Black
+	}
+
+	return theme.DefaultTheme().Color(name, variant)
+}
+
+func (m MyTheme) Icon(name fyne.ThemeIconName) fyne.Resource {
+	return theme.DefaultTheme().Icon(theme.IconNameHome)
+}
+
+func (m MyTheme) Font(style fyne.TextStyle) fyne.Resource {
+	return theme.DefaultTheme().Font(style)
+}
+
+func (m MyTheme) Size(name fyne.ThemeSizeName) float32 {
+	return theme.DefaultTheme().Size(name)
+}
