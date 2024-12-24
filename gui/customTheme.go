81daaef8779cf48b13de6719266882b641fe5abd
Andariel <workoanda@gmail.com>
2024-12-24 01:26:28 -0300

    Customize theme

--- gui/customTheme.go
+++ gui/customTheme.go
@@ -10,14 +10,24 @@ import (
 type MyTheme struct{}
 
 func (m MyTheme) Color(name fyne.ThemeColorName, variant fyne.ThemeVariant) color.Color {
-	if name == theme.ColorNameBackground {
-		if variant == theme.VariantLight {
-			return color.White
-		}
-		return color.Black
-	}
+	switch name {
+	case theme.ColorNameBackground:
+		return color.RGBA{R: 233, G: 242, B: 208, A: 0}
 
-	return theme.DefaultTheme().Color(name, variant)
+	case theme.ColorNameButton:
+		return color.White
+
+	case theme.ColorNameForeground:
+		return color.RGBA{R: 99, G: 145, B: 42, A: 200}
+
+	case theme.ColorNameSeparator:
+		return color.RGBA{R: 99, G: 145, B: 42, A: 200}
+
+		//		return color.RGBA{R: 99, G: 145, B: 42, A: 0}
+
+	default:
+		return theme.DefaultTheme().Color(name, variant)
+	}
 }
 
 func (m MyTheme) Icon(name fyne.ThemeIconName) fyne.Resource {
